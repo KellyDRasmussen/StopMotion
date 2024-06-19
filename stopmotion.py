ca57b9e83e889ca34f1aed653f751e41a8c4e523
Kelly Rasmussen <134787970+KellyDRasmussen@users.noreply.github.com>
2024-06-19 11:16:52 +0200

    Update stopmotion.py

--- stopmotion.py
+++ stopmotion.py
@@ -1,19 +1,17 @@
-# pyShutter.py
+# stop_motion.py
 # Read Bluetooth Remote Shutter
 # from /dev/input/event5 and /dev/input/event6
 # filter for key down of ENTER and VOLUMNUP
-#
-# http://helloraspberrypi.blogspot.com/2020/06/detect-bluetooth-remote-shutter-ab.html
-#
-# Work on Python 3.5+
-# For Reading events from multiple devices
-# read https://python-evdev.readthedocs.io/en/latest/tutorial.html
 
 from evdev import InputDevice, categorize, ecodes
 import asyncio
+import cv2
+from datetime import datetime
 
+# Define your camera and input devices
 Shutter5 = InputDevice('/dev/input/event5')
 Shutter6 = InputDevice('/dev/input/event6')
+camera = cv2.VideoCapture(0)  # Change the index if needed
 
 EV_VAL_PRESSED = 1
 EV_VAL_RELEASED = 0
@@ -24,18 +22,28 @@ print(Shutter5)
 print(Shutter6)
 print('=== Start ===')
 
+# Function to capture image
+def capture_image():
+    ret, frame = camera.read()
+    if ret:
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        file_path = f'/path/to/save/images/img_{timestamp}.jpg'  # Placeholder for file path
+        cv2.imwrite(file_path, frame)
+        print(f'Image saved to {file_path}')
+    else:
+        print('Failed to capture image')
+
 async def print_events(device):
-                
     async for event in device.async_read_loop():
-        if event.type == ecodes.EV_KEY:
-            if event.value == EV_VAL_PRESSED:
-                if event.value == EV_VAL_PRESSED:
-                    if event.code == KEY_ENTER:
-                        print('<ENTER> Pressed')
-                    elif event.code == KEY_VOLUMNUP:
-                        print('<VOLUMNUP> Pressed')
-                print(device.path, categorize(event), sep=': ')          
-                print('---')
+        if event.type == ecodes.EV_KEY and event.value == EV_VAL_PRESSED:
+            if event.code == KEY_ENTER:
+                print('<ENTER> Pressed')
+                capture_image()
+            elif event.code == KEY_VOLUMNUP:
+                print('<VOLUMNUP> Pressed')
+                capture_image()
+            print(device.path, categorize(event), sep=': ')
+            print('---')
 
 for device in Shutter5, Shutter6:
     asyncio.ensure_future(print_events(device))
@@ -43,3 +51,6 @@ for device in Shutter5, Shutter6:
 loop = asyncio.get_event_loop()
 loop.run_forever()
 
+# Release the camera when done
+camera.release()
+cv2.destroyAllWindows()
